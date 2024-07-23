24333099cfb30e32d4fee606beb08ddb9d4caeb8
ryanb <ryan.bilac2010@hotmail.com>
2024-07-23 13:24:05 -0300

    finalizado

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
 import express, { json } from "express"
 import { crudRouter } from "./router"
+import cors from 'cors'; 
 
 export const app = express()
 app.use(json())
-
-app.get("/health", (req, res) => {
-    return res.json("ola rapeize")
-})
+app.use(cors())
 
 app.listen(3000, () => console.log("ola mundo")
 )
